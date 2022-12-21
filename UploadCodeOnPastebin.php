ee9f563a92feba2e28a034c4121dbdcaf0f744c5
Vicky Kumar <imvickykumar999@gmail.com>
2022-12-21 16:02:50 +0530

    Update UploadCodeOnPastebin.php

--- UploadCodeOnPastebin.php
+++ UploadCodeOnPastebin.php
@@ -12,7 +12,7 @@ $headers = array(
 );
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-$data = "api_dev_key=80b366e17841431960b282260903afb6&api_paste_code=import math;print(math.pi)&api_paste_name=GitHub&api_paste_private=0&api_paste_expire_date=1Y&api_option=paste";
+$data = "api_dev_key=XXXXXXXXXXXXXXXXXXXXXXXX&api_paste_code=import math;print(math.pi)&api_paste_name=GitHub&api_paste_private=0&api_paste_expire_date=1Y&api_option=paste";
 
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 
