d255bff95e9662e25045d3882a2d3c500f48a64c
Vicky Kumar <imvickykumar999@gmail.com>
2022-12-21 16:02:23 +0530

    Update UploadCodeOnPastebin.py

--- UploadCodeOnPastebin.py
+++ UploadCodeOnPastebin.py
@@ -9,7 +9,7 @@ url = "https://pastebin.com/api/api_post.php"
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/x-www-form-urlencoded"
 
-data = "api_dev_key=80b366e17841431960b282260903afb6&api_paste_code=import math;print(math.pi)&api_paste_name=GitHub&api_paste_private=0&api_paste_expire_date=1Y&api_option=paste"
+data = "api_dev_key=XXXXXXXXXXXXXXXXXXXXXXXXX&api_paste_code=import math;print(math.pi)&api_paste_name=GitHub&api_paste_private=0&api_paste_expire_date=1Y&api_option=paste"
 
 resp = requests.post(url, headers=headers, data=data)
 
