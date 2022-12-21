3c7593b979ddc80ba79f58253cdfa840eda8d161
Vicky Kumar <imvickykumar999@gmail.com>
2022-12-21 16:35:02 +0530

    Update UploadCodeOnPastebin.py

--- UploadCodeOnPastebin.py
+++ UploadCodeOnPastebin.py
@@ -1,6 +1,4 @@
 
-# https://reqbin.com/c-gjforbia
-
 import requests
 from requests.structures import CaseInsensitiveDict
 
@@ -9,8 +7,11 @@ url = "https://pastebin.com/api/api_post.php"
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/x-www-form-urlencoded"
 
-data = "api_dev_key=XXXXXXXXXXXXXXXXXXXXXXXXX&api_paste_code=import math;print(math.pi)&api_paste_name=GitHub&api_paste_private=0&api_paste_expire_date=1Y&api_option=paste"
+data = '''api_dev_key=XXXXXXXXXXXXXXXXXXXXXXX&api_paste_name=GitHub&api_paste_private=0&api_paste_expire_date=1Y&api_option=paste&api_paste_code=
+import math
+print(math.pi)
+'''
 
 resp = requests.post(url, headers=headers, data=data)
 
-print(resp.status_code)
+print(resp.text)
